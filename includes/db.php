e03ad81ca179d0071b65053fd47d7f92a4429649
Mustata <umbrroxtz@yahoo.com>
2023-08-06 00:13:33 +0300

    db

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,13 @@
 <?php ob_start();
 
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = '';
+$db['db_host'] = 'app-8f4568c4-00b3-4455-86ef-3c6d92116739-do-user-14301607-0.b.db.ondigitalocean.com';
+$db['db_user'] = 'db';
+$db['db_pass'] = 'AVNS_2G7s7YsFxUm8pQTV7ke';
 $db['db_name'] = 'cms';
+$db['port'] = '25060';
+$db['database'] = 'db';
+$db['sslmode'] = 'require';
+
 
 foreach($db as $key => $value) {
     define(strtoupper($key), $value);
